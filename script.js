1de408beb97cbbaa2d6bda14785d784008be47f5
Christopher Garcia <chrisgarcia0903@gmail.com>
2021-10-19 18:31:14 -0400

    Added button and time into js and created startGame function

---
+++ script.js
@@ -0,0 +1,11 @@
+var startBtn = document.querySelector("#startbutton");
+var timeEl = document.querySelector(".time");
+
+// var startGame = function () {
+//     console.log("hello world")
+// }
+console.log("hello world")
+startBtn.addEventListener("click", function(){
+    
+});
+
