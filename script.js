a29521eace146ce9fdda289861e2bb8eb531539e
Christopher Garcia <chrisgarcia0903@gmail.com>
2021-10-19 19:20:23 -0400

    made when start button clicked, intro becomes hidden and answer choices display

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 var startBtn = document.querySelector("#startbutton");
 var timeEl = document.querySelector(".time");
 var intro = document.querySelector(".intro");
-var choices = document.querySelector("answer-choices")
+var choices = document.querySelector("#questions");
 var questions = ["what is 5 + 5?", 'what is the opposite of up?', 'what number comes before 25?', 'what ']
 
 
 startBtn.addEventListener("click", function(){
-    console.log('started game')
-    intro.classList.add('hide')
+    console.log('started game');
+    intro.classList.add("hide");
+    choices.classList.remove('hide');
+    
 });
 
 
