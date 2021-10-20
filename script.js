d82514df887a4e62ec274dfb32070ec1d1c82f43
Christopher Garcia <chrisgarcia0903@gmail.com>
2021-10-19 23:57:38 -0400

    Attempted to create question and answer choices when game starts

--- script.js
+++ script.js
@@ -2,8 +2,52 @@ var startBtn = document.querySelector("#startbutton");
 var timeEl = document.querySelector(".time");
 var intro = document.querySelector(".intro");
 var choices = document.querySelector("#container");
-var questions = ["what is 5 + 5?", 'what is the opposite of up?', 'what number comes before 25?', 'what ']
-var timeleft = 5;
+var timeleft = 10000;
+// const questions = [
+//     {
+//       question: 'What is 2 + 2?',
+//       answers: [
+//         { text: '16', correct: false },
+//         { text: 'yes', correct: false },
+//         { text: '1', correct: false },
+//         { text: '4', correct: true }
+//       ]
+//     },
+//     // {
+//     //   question: 'What is the opposite of down?',
+//     //   answers: [
+//     //     { text: 'up', correct: true },
+//     //     { text: 'left', correct: false },
+//     //     { text: '3', correct: false },
+//     //     { text: 'apples', correct: false }
+//     //   ]
+//     // },
+//     // {
+//     //   question: 'What number comes before 25?',
+//     //   answers: [
+//     //     { text: '26', correct: true },
+//     //     { text: 'yellow', correct: false },
+//     //     { text: '24', correct: false },
+//     //     { text: 'no', correct: false }
+//     //   ]
+//     // },
+//     // {
+//     //   question: 'What is 25 * 1823',
+//     //   answers: [
+//     //     { text: 'I dont know!!!!', correct: true },
+//     //     { text: '1', correct: false },
+//     //     { text: '100', correct: false },
+//     //     { text: '10,000', correct: false }
+//     //   ]
+//     // }
+//   ]
+const questions = ["What is 2+2?", "What is", "what isn't"]
+const answers = [ "4", "5", "6", "7"]
+const questionEL = document.getElementById("question")
+const answerBtnEl = document.getElementById("answer-choices")
+var questionNum = questions.length
+console.log(questionNum)
+
 
 startBtn.addEventListener("click", function(){
     console.log('started game');
@@ -17,7 +61,13 @@ startBtn.addEventListener("click", function(){
         document.getElementById("time-left").innerHTML = timeleft;
         timeleft -= 1;
       }, 1000);
-
+    
+     firstQuestion()
 })
+function firstQuestion() {
+    questionEL.innerText = questions[0]
+        const button = document.createElement("button")
+        button.innerText = "hello"
+        button.classList.add("answers")
 
-
+    }
