e4140ab3c13e3f5fed3d9accf995ae7b9945d262
Christopher Garcia <chrisgarcia0903@gmail.com>
2021-10-20 23:08:54 -0400

    Added all questions to quiz

--- script.js
+++ script.js
@@ -4,45 +4,6 @@ var intro = document.querySelector(".intro");
 var choices = document.querySelector("#container");
 var timeleft = 10000;
 var answerBtn = document.querySelector('.answers')
-// const questions = [
-//     {
-//       question: 'What is 2 + 2?',
-//       answers: [
-//         { text: '16', correct: false },
-//         { text: 'yes', correct: false },
-//         { text: '1', correct: false },
-//         { text: '4', correct: true }
-//       ]
-//     },
-//     // {
-//     //   question: 'What is the opposite of down?',
-//     //   answers: [
-//     //     { text: 'up', correct: true },
-//     //     { text: 'left', correct: false },
-//     //     { text: '3', correct: false },
-//     //     { text: 'apples', correct: false }
-//     //   ]
-//     // },
-//     // {
-//     //   question: 'What number comes before 25?',
-//     //   answers: [
-//     //     { text: '26', correct: true },
-//     //     { text: 'yellow', correct: false },
-//     //     { text: '24', correct: false },
-//     //     { text: 'no', correct: false }
-//     //   ]
-//     // },
-//     // {
-//     //   question: 'What is 25 * 1823',
-//     //   answers: [
-//     //     { text: 'I dont know!!!!', correct: true },
-//     //     { text: '1', correct: false },
-//     //     { text: '100', correct: false },
-//     //     { text: '10,000', correct: false }
-//     //   ]
-//     // }
-//   ]
-
 var question1 = {
     question: "What is 2+2?",
     answers: [
@@ -61,14 +22,29 @@ var question2 = {
                 { text: 'back', correct: false },
     ]
 }
+var question3 = {
+    question: "What is 5 * 5",
+    answers: [
+        { text: '10', correct: false },
+        { text: '25', correct: true },
+        { text: '100', correct: false },
+        { text: '0', correct: false },
+    ]
+}
+var question4 = {
+    question: "What number comes before 25",
+    answers: [
+        { text: '24', correct: true },
+        { text: '18', correct: false },
+        { text: '26', correct: false },
+        { text: '52', correct: false },
+    ]
+}
 
-// const questions = ["What is 2+2?", "What is", "what isn't"]
-// const answers = [ "4", "5", "6", "7"]
+
 const questionEL = document.getElementById("question")
 const answerBtnEl = document.getElementById("answer-choices")
-// var questionNum = questions.length
-// console.log(questionNum)
-// var answerBtn = document.querySelector(".answers")
+
 
 startBtn.addEventListener("click", function(){
     intro.classList.add("hide");
@@ -91,20 +67,17 @@ function firstQuestion() {
         button.classList.add("answers")
         button.innerText = question1.answers[i].text;
         answerBtnEl.append(button); 
-        button.value = question1.answers[i].correct;
-        console.log(button.value)
+        button.value = question1.answers[i].correct;    
     }
-    console.log(button.value)
     answerBtnEl.addEventListener('click', function(event){
         var selectedAnswer = event.target.value
         if (selectedAnswer === "true") {
             alert("you got the right answer");
-            setNextQuestions();
         }
         else {
             alert("You are wrong");
-            setNextQuestions();
         }
+        setNextQuestions()
     })
     
 }
@@ -118,22 +91,65 @@ function setNextQuestions() {
         button.classList.add("answers")
         button.innerText = question2.answers[i].text;
         answerBtnEl.append(button); 
-        button.value = question2.answers[i].correct;
-        console.log(button.value)
+        button.value = question2.answers[i].correct;    
     }
-    console.log(button.value)
     answerBtnEl.addEventListener('click', function(event){
         var selectedAnswer = event.target.value
-    
-
-        if (selectedAnswer === "true") {
-            alert("you got the right answer");
-            setNextQuestions();
+     if (selectedAnswer === "true") {
+            // alert("you got the right answer");
+            setNextQuestions2();
         }
         else {
-            alert("You are wrong");
-            setNextQuestions();
+            // alert("You are wrong");
+            setNextQuestions2();
         }
     })
-    
+}
+function setNextQuestions2() {
+    while (answerBtnEl.firstChild) {
+        answerBtnEl.removeChild(answerBtnEl.firstChild)
+      }
+    questionEL.innerText = question3.question;
+    for (var i = 0; i < question3.answers.length; i++) {
+        var button = document.createElement("button")
+        button.classList.add("answers")
+        button.innerText = question3.answers[i].text;
+        answerBtnEl.append(button); 
+        button.value = question3.answers[i].correct;    
+    }
+    answerBtnEl.addEventListener('click', function(event){
+        var selectedAnswer = event.target.value
+        if (selectedAnswer === "true") {
+
+        }
+        else {
+           
+        }
+        setNextQuestions3()
+    })
+}
+function setNextQuestions3() {
+    while (answerBtnEl.firstChild) {
+        answerBtnEl.removeChild(answerBtnEl.firstChild)
+      }
+    questionEL.innerText = question4.question;
+    for (var i = 0; i < question4.answers.length; i++) {
+        var button = document.createElement("button")
+        button.classList.add("answers")
+        button.innerText = question4.answers[i].text;
+        answerBtnEl.append(button); 
+        button.value = question4.answers[i].correct;    
+    }
+    answerBtnEl.addEventListener('click', function(event){
+        var selectedAnswer = event.target.value
+        if (selectedAnswer === "true") {
+           
+        }
+        else {
+        }
+        finalScreen()
+    })
+}
+function finalScreen(){
+    choices.classList.add('hide');
 }
